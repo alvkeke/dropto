45f8c1a89739508fcd76eca993d14efcac30c029
alvkeke@126.com <alvkeke@126.com>
2024-03-08 01:05:00 +0800

    [ui] add share to handle activity

---
+++ app/src/main/java/cn/alvkeke/dropto/ui/ShareRecvActivity.java
@@ -0,0 +1,17 @@
+package cn.alvkeke.dropto.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cn.alvkeke.dropto.R;
+
+public class ShareRecvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_recv);
+
+    }
+}
