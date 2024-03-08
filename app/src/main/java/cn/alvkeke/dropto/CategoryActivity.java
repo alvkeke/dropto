4708c6f072b9f410f29976515d81f26e1ff072f2
alvkeke <alvkeke@126.com>
2024-03-08 20:29:49 +0800

    [misc] add more basic files for CategoryActivity and CategoryList

--- app/src/main/java/cn/alvkeke/dropto/CategoryActivity.java
+++ app/src/main/java/cn/alvkeke/dropto/CategoryActivity.java
@@ -1,14 +1,20 @@
 package cn.alvkeke.dropto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class CategoryActivity extends AppCompatActivity {
 
+    private RecyclerView rlCategory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        rlCategory = findViewById(R.id.rlist_category);
+
     }
 }
