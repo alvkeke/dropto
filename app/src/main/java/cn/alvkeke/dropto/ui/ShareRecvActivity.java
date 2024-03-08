41a02a330c1bc4482be07818036c9604f99d8042
alvkeke@126.com <alvkeke@126.com>
2024-03-09 01:19:24 +0800

    [feature] implement basic function of share to function.

--- app/src/main/java/cn/alvkeke/dropto/ui/ShareRecvActivity.java
+++ app/src/main/java/cn/alvkeke/dropto/ui/ShareRecvActivity.java
@@ -1,10 +1,20 @@
 package cn.alvkeke.dropto.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import cn.alvkeke.dropto.R;
+import cn.alvkeke.dropto.data.Category;
+import cn.alvkeke.dropto.data.Global;
 
 public class ShareRecvActivity extends AppCompatActivity {
 
@@ -13,5 +23,25 @@ public class ShareRecvActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_recv);
 
+        RecyclerView rlCategory = findViewById(R.id.rlist_share_recv);
+        ArrayList<Category> categories = Global.getInstance().getCategories();
+        CategoryListAdapter adapter = new CategoryListAdapter(categories);
+        rlCategory.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rlCategory.setLayoutManager(layoutManager);
+        rlCategory.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        adapter.setItemClickListener(new CategoryListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int index, View v) {
+                Log.e(this.toString(), "action: " + action);
+                Log.e(this.toString(), "type: " + type);
+            }
+        });
     }
 }
