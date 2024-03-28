041971fcf6468955a5452ab43bff0ea08895b45d
alvkeke@126.com <alvkeke@126.com>
2024-03-28 15:00:59 +0800

    [misc] remove LauncherActivity, make fragments are all managed by MainActivity

--- app/src/main/java/cn/alvkeke/dropto/ui/activity/MainActivity.java
+++ app/src/main/java/cn/alvkeke/dropto/ui/activity/MainActivity.java
@@ -2,10 +2,14 @@ package cn.alvkeke.dropto.ui.activity;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,16 +18,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.viewpager2.widget.ViewPager2;
 
+import java.io.File;
+import java.io.FileDescriptor;
 import java.util.ArrayList;
+import java.util.List;
 
+import cn.alvkeke.dropto.BuildConfig;
 import cn.alvkeke.dropto.R;
 import cn.alvkeke.dropto.data.Category;
 import cn.alvkeke.dropto.data.Global;
 import cn.alvkeke.dropto.data.NoteItem;
+import cn.alvkeke.dropto.debug.DebugFunction;
 import cn.alvkeke.dropto.storage.DataBaseHelper;
+import cn.alvkeke.dropto.storage.FileHelper;
 import cn.alvkeke.dropto.ui.adapter.MainFragmentAdapter;
 import cn.alvkeke.dropto.ui.fragment.CategoryDetailFragment;
 import cn.alvkeke.dropto.ui.fragment.CategoryListFragment;
+import cn.alvkeke.dropto.ui.fragment.CategorySelectorFragment;
 import cn.alvkeke.dropto.ui.fragment.NoteDetailFragment;
 import cn.alvkeke.dropto.ui.fragment.NoteListFragment;
 import cn.alvkeke.dropto.ui.intf.ListNotification;
@@ -35,11 +46,180 @@ public class MainActivity extends AppCompatActivity implements
     private ViewPager2 viewPager;
     private MainFragmentAdapter fragmentAdapter;
 
+    private void initCategoryList() {
+        Global global = Global.getInstance();
+        ArrayList<Category> categories = global.getCategories();
+
+        File img_folder = this.getExternalFilesDir("imgs");
+        assert img_folder != null;
+        if (!img_folder.exists()) {
+            Log.i(this.toString(), "image folder not exist, create: " + img_folder.mkdir());
+        }
+        global.setFileStoreFolder(img_folder);
+
+        // TODO: for debug only, remember to remove.
+        if (BuildConfig.DEBUG) {
+            DebugFunction.fill_database_for_category(this);
+            List<File> img_files = DebugFunction.try_extract_res_images(this, img_folder);
+            DebugFunction.fill_database_for_note(this, img_files, 1);
+        }
+
+        // retrieve all categories from database always, since they will not take up
+        // too many memory
+        try (DataBaseHelper dbHelper = new DataBaseHelper(this)) {
+            dbHelper.start();
+            dbHelper.queryCategory(-1, categories);
+            dbHelper.finish();
+        } catch (Exception e) {
+            Log.e(this.toString(), "failed to retrieve data from database:" + e);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if (action == null) {
+            finish();
+            return;
+        }
+
+        initCategoryList();
+
+        switch (action) {
+            case Intent.ACTION_MAIN:
+                onCreateMain(savedInstanceState);
+                break;
+            case Intent.ACTION_SEND:
+                onCreateSend(intent);
+                break;
+            default:
+                finish();
+        }
+    }
+
+    private void onCreateSend(Intent intent) {
+        NoteItem recvNote = handleSharedInfo(intent);
+        if (recvNote == null) {
+            Toast.makeText(this, "Failed to create new item",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        getSupportFragmentManager().beginTransaction()
+                .add(new CategorySelectorFragment(this, new ShareRecvHandler(recvNote)), null)
+                .commit();
+    }
+
+    class ShareRecvHandler implements CategorySelectorFragment.CategorySelectListener {
+
+        private final NoteItem recvNote;
+        ShareRecvHandler(NoteItem recvNote) {
+            this.recvNote = recvNote;
+        }
+
+        @Override
+        public void onSelected(int index, Category category) {
+            handleNoteCreate(category, recvNote);
+            finish();
+        }
+
+        @Override
+        public void onError(String error) {
+            Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    NoteItem handleSharedInfo(Intent intent) {
+        String type = intent.getType();
+
+        if (type == null) {
+            Log.e(this.toString(), "Cannot get type");
+            Toast.makeText(this, "Cannot get type", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        if (type.startsWith("text/")) {
+            return handleText(intent);
+        } else if (type.startsWith("image/")) {
+            return handleImage(intent);
+        } else {
+            Log.e(this.toString(), "Got unsupported type: " + type);
+        }
+        return null;
+    }
+
+    NoteItem handleText(Intent intent) {
+        String text = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (text == null) {
+            Log.e(this.toString(), "Failed to get shared text");
+            return null;
+        }
+
+        return new NoteItem(text);
+    }
+
+    // TODO: seems ugly implementation, seek if there is better implementation
+    private String getFileNameFromUri(Uri uri) {
+        // ContentResolver to resolve the content Uri
+        ContentResolver resolver = this.getContentResolver();
+        // Query the file name from the content Uri
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        String fileName = null;
+        if (cursor != null && cursor.moveToFirst()) {
+            int index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            if (index != -1) {
+                fileName = cursor.getString(index);
+            }
+            cursor.close();
+        }
+        return fileName;
+    }
+
+    NoteItem handleImage(Intent intent) {
+        File storeFolder = Global.getInstance().getFileStoreFolder();
+        if (storeFolder == null) {
+            Log.e(this.toString(), "Failed to get storage folder");
+            return null;
+        }
+
+        Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (uri == null) {
+            Log.e(this.toString(), "Failed to get Uri");
+            return null;
+        }
+
+        try (ParcelFileDescriptor inputPFD = this.getContentResolver().openFileDescriptor(uri, "r")) {
+            if (inputPFD == null) {
+                Log.e(this.toString(), "Failed to get ParcelFileDescriptor");
+                return null;
+            }
+            FileDescriptor fd = inputPFD.getFileDescriptor();
+            byte[] md5sum = FileHelper.calculateMD5(fd);
+            File retFile = FileHelper.md5ToFile(storeFolder, md5sum);
+            if (retFile.isFile() && retFile.exists()) {
+                Log.d(this.toString(), "File exist");
+            } else {
+                Log.d(this.toString(), "Save file to : " + retFile.getAbsolutePath());
+                FileHelper.copyFileTo(fd, retFile);
+            }
+            String ext_str = intent.getStringExtra(Intent.EXTRA_TEXT);
+            NoteItem item = new NoteItem(ext_str == null ? "" : ext_str);
+            item.setImageFile(retFile);
+            item.setImageName(getFileNameFromUri(uri));
+            return item;
+        } catch (Exception e) {
+            Log.e(this.toString(), "Failed to store shared file: " + e);
+        }
+        return null;
+    }
+
+    private void onCreateMain(Bundle savedInstanceState) {
+        Log.e(this.toString(), "enter MAIN mode");
+        setTheme(R.style.Theme_DropTo_NoActionBar);
         setContentView(R.layout.activity_main);
-        Log.e(this.toString(), "theme: " + getTheme());
 
         getOnBackPressedDispatcher().
                 addCallback(this, new OnFragmentBackPressed(true));
@@ -233,6 +413,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void handleNoteCreate(Category c, NoteItem newItem) {
+        newItem.setCategoryId(c.getId());
         try (DataBaseHelper dbHelper = new DataBaseHelper(this)) {
             dbHelper.start();
             newItem.setId(dbHelper.insertNote(newItem));
@@ -243,8 +424,9 @@ public class MainActivity extends AppCompatActivity implements
         }
 
         int index = c.addNoteItem(newItem);
-        fragmentAdapter.getNoteListFragment().notifyItemListChanged(
-                ListNotification.Notify.CREATED, index, newItem);
+        NoteListFragment fragment = fragmentAdapter.getNoteListFragment();
+        if (fragment == null) return;
+        fragment.notifyItemListChanged(ListNotification.Notify.CREATED, index, newItem);
     }
 
     private void handleNoteRemove(Category c, NoteItem e) {
