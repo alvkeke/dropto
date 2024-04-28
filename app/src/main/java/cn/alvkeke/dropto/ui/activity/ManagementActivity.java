63ecec7d415dfcc37c5d9324f6b1bd845eb63277
alvkeke <alvkeke@126.com>
2024-04-29 00:11:21 +0800

    [ui] update layout of ManagementActivity, add ViewPager

--- app/src/main/java/cn/alvkeke/dropto/ui/activity/ManagementActivity.java
+++ app/src/main/java/cn/alvkeke/dropto/ui/activity/ManagementActivity.java
@@ -17,11 +17,6 @@ public class ManagementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_management);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
     }
 }
