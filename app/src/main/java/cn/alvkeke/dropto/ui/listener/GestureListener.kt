babae105648b85f98870b406e3ee1a73df001ac4
alvkeke <15010199+alvkeke@users.noreply.github.com>
2025-12-08 00:43:11 +0800

    [misc/refactor] convert GestureListener to kotlin

--- app/src/main/java/cn/alvkeke/dropto/ui/listener/GestureListener.kt
+++ app/src/main/java/cn/alvkeke/dropto/ui/listener/GestureListener.kt
@@ -1,41 +1,39 @@
-package cn.alvkeke.dropto.ui.listener;
+package cn.alvkeke.dropto.ui.listener
 
-import android.annotation.SuppressLint;
-import android.os.Handler;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.MotionEvent.PointerCoords;
-import android.view.View;
+import android.annotation.SuppressLint
+import android.os.Handler
+import android.util.Log
+import android.view.MotionEvent
+import android.view.MotionEvent.PointerCoords
+import android.view.View
+import android.view.View.OnTouchListener
+import kotlin.math.abs
+import kotlin.math.sqrt
 
-public class GestureListener implements View.OnTouchListener{
-
-    private static class Point {
-        float x;
-        float y;
-        public Point() {
+open class GestureListener : OnTouchListener {
+    private class Point {
+        var x: Float = 0f
+        var y: Float = 0f
+        fun set(x: Float, y: Float) {
+            this.x = x
+            this.y = y
         }
 
-        public void set(float x, float y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @SuppressWarnings("unused")
-        public float distance(Point point) {
-            float xx, yy;
-            xx = point.x - x;
-            yy = point.y - y;
-            return (float) Math.sqrt(xx*xx + yy*yy);
+        @Suppress("unused")
+        fun distance(point: Point): Float {
+            val xx: Float = point.x - x
+            val yy: Float = point.y - y
+            return sqrt((xx * xx + yy * yy).toDouble()).toFloat()
         }
     }
 
-    private Point pointFromEvent(MotionEvent e) {
-        Point point = new Point();
-        point.set(e.getRawX(), e.getRawY());
-        return point;
+    private fun pointFromEvent(e: MotionEvent): Point {
+        val point = Point()
+        point.set(e.rawX, e.rawY)
+        return point
     }
 
-    private enum GestureMode {
+    private enum class GestureMode {
         NONE,
         SCROLL_V,
         SCROLL_H,
@@ -44,206 +42,222 @@ public class GestureListener implements View.OnTouchListener{
         INVALID,
     }
 
-    private static final int DISTANCE_NO_MOVE = 25;
-    private static final int DISTANCE_SCROLL = 45;
-    private GestureMode getMode(float deltaX, float deltaY) {
+    private fun getMode(deltaX: Float, deltaY: Float): GestureMode {
         if (deltaX <= DISTANCE_NO_MOVE && deltaY >= DISTANCE_SCROLL) {
-            return GestureMode.SCROLL_V;
+            return GestureMode.SCROLL_V
         } else if (deltaX >= DISTANCE_SCROLL && deltaY <= DISTANCE_NO_MOVE) {
-            return GestureMode.SCROLL_H;
+            return GestureMode.SCROLL_H
         } else if (deltaX >= DISTANCE_NO_MOVE && deltaY >= DISTANCE_NO_MOVE) {
-            return GestureMode.DRAG;
+            return GestureMode.DRAG
         }
-        return GestureMode.NONE;
-    }
-    private GestureMode getMode(Point p1, Point p2) {
-        float dx, dy;
-        dx = p1.x - p2.x;
-        dy = p1.y - p2.y;
-        return getMode(Math.abs(dx), Math.abs(dy));
+        return GestureMode.NONE
     }
 
-
-    private Point pointOld;
-    private void handleGesture(View view, GestureMode mode, MotionEvent event) {
-        boolean ret = false;
-        Point current = pointFromEvent(event);
-        if (mode == GestureMode.SCROLL_H)
-            ret = onScrollHorizontal(view, current.x - pointOld.x);
-        else if (mode == GestureMode.SCROLL_V)
-            ret = onScrollVertical(view, current.y - pointOld.y);
-        if (!ret)
-            onDrag(view, current.x - pointOld.x, current.y - pointOld.y);
-        pointOld = current;
+    private fun getMode(p1: Point, p2: Point): GestureMode {
+        val dx: Float = p1.x - p2.x
+        val dy: Float = p1.y - p2.y
+        return getMode(abs(dx), abs(dy))
     }
 
-    private void handleGestureEnd(View view, MotionEvent event, GestureMode mode) {
-        boolean ret = false;
-        if (mode == GestureMode.SCROLL_H)
-            ret = onScrollHorizontalEnd(view, event);
-        else if (mode == GestureMode.SCROLL_V)
-            ret = onScrollVerticalEnd(view, event);
-        if (!ret)
-            onDragEnd(view, event);
+
+    private lateinit var pointOld: Point
+    private fun handleGesture(view: View, mode: GestureMode, event: MotionEvent) {
+        var ret = false
+        val current = pointFromEvent(event)
+        if (mode == GestureMode.SCROLL_H) {
+            ret = onScrollHorizontal(view, current.x - pointOld.x)
+        } else if (mode == GestureMode.SCROLL_V) {
+            ret = onScrollVertical(view, current.y - pointOld.y)
+        }
+        if (!ret) {
+            onDrag(view, current.x - pointOld.x, current.y - pointOld.y)
+        }
+        pointOld = current
     }
 
-    private float getCurrentDistance(MotionEvent motionEvent) {
-        PointerCoords c1 = new PointerCoords();
-        PointerCoords c2 = new PointerCoords();
-        motionEvent.getPointerCoords(0, c1);
-        motionEvent.getPointerCoords(1, c2);
-        float xx = c1.x - c2.x;
-        float yy = c1.y - c2.y;
-        return (float) Math.sqrt(xx*xx + yy*yy);
+    private fun handleGestureEnd(view: View, event: MotionEvent, mode: GestureMode) {
+        var ret = false
+        if (mode == GestureMode.SCROLL_H) {
+            ret = onScrollHorizontalEnd(view, event)
+        } else if (mode == GestureMode.SCROLL_V) {
+            ret = onScrollVerticalEnd(view, event)
+        }
+        if (!ret) {
+            onDragEnd(view, event)
+        }
     }
 
-    private float oldDist;
-    private void handleZoom(View view, MotionEvent motionEvent) {
-        float newDist = getCurrentDistance(motionEvent);
-        float ratio = newDist / oldDist;
-        oldDist = newDist;
-        onZoom(view, ratio);
+    private fun getCurrentDistance(motionEvent: MotionEvent): Float {
+        val c1 = PointerCoords()
+        val c2 = PointerCoords()
+        motionEvent.getPointerCoords(0, c1)
+        motionEvent.getPointerCoords(1, c2)
+        val xx = c1.x - c2.x
+        val yy = c1.y - c2.y
+        return sqrt((xx * xx + yy * yy).toDouble()).toFloat()
     }
 
-    private void handleZoomEnd(View view) {
-        onZoomEnd(view);
+    private var oldDist = 0f
+    private fun handleZoom(view: View, motionEvent: MotionEvent) {
+        val newDist = getCurrentDistance(motionEvent)
+        val ratio = newDist / oldDist
+        oldDist = newDist
+        onZoom(view, ratio)
     }
 
-    private final Handler handler = new Handler();
+    private fun handleZoomEnd(view: View) {
+        onZoomEnd(view)
+    }
 
-    private static final long CLICK_TIME_THRESHOLD = 200;
-    private long downTime;
-    private int downCount = 0;
-    private void handleClick(View view, MotionEvent e) {
-        if (downCount > 1) return;
+    private val handler = Handler()
+
+    private var downTime: Long = 0
+    private var downCount = 0
+    private fun handleClick(view: View, e: MotionEvent) {
+        if (downCount > 1) return
         if (System.currentTimeMillis() - downTime < CLICK_TIME_THRESHOLD) {
-            clickView = view;
-            clickEvent = e;
-            handler.postDelayed(SingleTapRunnable, CLICK_TIME_THRESHOLD);
+            clickView = view
+            clickEvent = e
+            handler.postDelayed(singleTapRunnable, CLICK_TIME_THRESHOLD)
         } else {
-            downCount = 0;
+            downCount = 0
         }
     }
 
-    private View clickView;
-    private MotionEvent clickEvent;
-    private final Runnable SingleTapRunnable = () -> {
-        downCount = 0;
-        onClick(clickView, clickEvent);
-    };
-
-    private void handleDoubleTap(View view, MotionEvent e) {
-        downCount = 0;
-        onDoubleClick(view, e);
+    private lateinit var clickView: View
+    private lateinit var clickEvent: MotionEvent
+    private val singleTapRunnable = Runnable {
+        downCount = 0
+        onClick(clickView, clickEvent)
     }
 
-    private final Point pointDown = new Point();
-    GestureMode mode;
+    private fun handleDoubleTap(view: View, e: MotionEvent) {
+        downCount = 0
+        onDoubleClick(view, e)
+    }
+
+    private val pointDown = Point()
+    private var mode: GestureMode = GestureMode.NONE
+
     @SuppressLint("ClickableViewAccessibility")
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        Point p;
-        switch (motionEvent.getActionMasked()) {
-            case MotionEvent.ACTION_POINTER_DOWN:
-                downCount = 0;
-                if (mode != GestureMode.NONE && mode != GestureMode.ZOOM) return true;
-                if (motionEvent.getPointerCount() == 2) {
-                    oldDist = getCurrentDistance(motionEvent);
-                    mode = GestureMode.ZOOM;
+    override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+        val p: Point?
+        when (motionEvent.actionMasked) {
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                downCount = 0
+                if (mode != GestureMode.NONE && mode != GestureMode.ZOOM) return true
+                if (motionEvent.pointerCount == 2) {
+                    oldDist = getCurrentDistance(motionEvent)
+                    mode = GestureMode.ZOOM
                 } else {
-                    Log.e(this.toString(), "INVALID COUNT: " + motionEvent.getPointerCount());
-                    mode = GestureMode.INVALID;
+                    Log.e(this.toString(), "INVALID COUNT: " + motionEvent.pointerCount)
+                    mode = GestureMode.INVALID
                 }
-                break;
-            case MotionEvent.ACTION_DOWN:
-                pointDown.set(motionEvent.getRawX(), motionEvent.getRawY());
-                pointOld = pointDown;
-                downTime = System.currentTimeMillis();
-                downCount++;
-                handler.removeCallbacks(SingleTapRunnable);
+            }
+
+            MotionEvent.ACTION_DOWN -> {
+                pointDown.set(motionEvent.rawX, motionEvent.rawY)
+                pointOld = pointDown
+                downTime = System.currentTimeMillis()
+                downCount++
+                handler.removeCallbacks(singleTapRunnable)
                 if (downCount > 1) {
-                    handleDoubleTap(view, motionEvent);
-                    mode = GestureMode.INVALID;
-                    return true;
+                    handleDoubleTap(view, motionEvent)
+                    mode = GestureMode.INVALID
+                    return true
                 }
-                mode = GestureMode.NONE;
-                break;
-            case MotionEvent.ACTION_MOVE:
+                mode = GestureMode.NONE
+            }
+
+            MotionEvent.ACTION_MOVE -> {
                 if (mode == GestureMode.INVALID) {
-                    downCount = 0;
-                    return true;
+                    downCount = 0
+                    return true
                 } else if (mode == GestureMode.ZOOM) {
-                    downCount = 0;
-                    handleZoom(view, motionEvent);
-                    return true;
+                    downCount = 0
+                    handleZoom(view, motionEvent)
+                    return true
                 } else if (mode != GestureMode.NONE) {
-                    downCount = 0;
-                    handleGesture(view, mode, motionEvent);
-                    return true;
+                    downCount = 0
+                    handleGesture(view, mode, motionEvent)
+                    return true
                 }
-                p = pointFromEvent(motionEvent);
-                mode = getMode(pointDown, p);
-                break;
-            case MotionEvent.ACTION_UP:
-                if (mode == GestureMode.NONE) {
-                    if (downCount == 1) {
-                        handleClick(view, motionEvent);
-                    }
-                } else if (mode == GestureMode.ZOOM || mode == GestureMode.INVALID) {
-                    return true;
-                } else {
-                    handleGestureEnd(view, motionEvent, mode);
+                p = pointFromEvent(motionEvent)
+                mode = getMode(pointDown, p)
+            }
+
+            MotionEvent.ACTION_UP -> if (mode == GestureMode.NONE) {
+                if (downCount == 1) {
+                    handleClick(view, motionEvent)
                 }
-                break;
-            case MotionEvent.ACTION_POINTER_UP:
-                if (mode == GestureMode.ZOOM && motionEvent.getPointerCount() <= 2) {
-                    handleZoomEnd(view);
-                    mode = GestureMode.INVALID;
-                }
-                break;
-            default:
-                Log.e(this.toString(), "action: " + motionEvent.getAction() + ", " + motionEvent.getActionMasked());
+            } else if (mode == GestureMode.ZOOM || mode == GestureMode.INVALID) {
+                return true
+            } else {
+                handleGestureEnd(view, motionEvent, mode)
+            }
+
+            MotionEvent.ACTION_POINTER_UP -> if (mode == GestureMode.ZOOM && motionEvent.pointerCount <= 2) {
+                handleZoomEnd(view)
+                mode = GestureMode.INVALID
+            }
+
+            else -> Log.e(
+                this.toString(),
+                "action: " + motionEvent.action + ", " + motionEvent.actionMasked
+            )
         }
-        return true;
+        return true
     }
 
 
-    @SuppressWarnings("unused")
-    public void onClick(View v, MotionEvent e){ }
-
-    @SuppressWarnings("unused")
-    public void onDoubleClick(View v, MotionEvent e){ }
-
-    @SuppressWarnings("unused")
-    public boolean onScrollVertical(View view, float deltaY) {
-        return false;
+    @Suppress("unused")
+    open fun onClick(v: View, e: MotionEvent) {
     }
 
-    @SuppressWarnings("unused")
-    public boolean onScrollVerticalEnd(View view, MotionEvent motionEvent) {
-        return false;
+    @Suppress("unused")
+    open fun onDoubleClick(v: View, e: MotionEvent) {
     }
 
-    @SuppressWarnings("unused")
-    public boolean onScrollHorizontal(View view, float deltaX) {
-        return false;
+    @Suppress("unused")
+    open fun onScrollVertical(view: View, deltaY: Float): Boolean {
+        return false
     }
 
-    @SuppressWarnings("unused")
-    public boolean onScrollHorizontalEnd(View view, MotionEvent motionEvent) {
-        return false;
+    @Suppress("unused")
+    open fun onScrollVerticalEnd(view: View, motionEvent: MotionEvent): Boolean {
+        return false
     }
 
-    @SuppressWarnings("unused")
-    public void onDrag(View view, float deltaX, float deltaY) { }
+    @Suppress("unused")
+    open fun onScrollHorizontal(view: View, deltaX: Float): Boolean {
+        return false
+    }
 
-    @SuppressWarnings("unused")
-    public void onDragEnd(View view, MotionEvent motionEvent) { }
+    @Suppress("unused")
+    open fun onScrollHorizontalEnd(view: View, motionEvent: MotionEvent): Boolean {
+        return false
+    }
 
-    @SuppressWarnings("unused")
-    public void onZoom(View view, float ratio) { }
+    @Suppress("unused")
+    open fun onDrag(view: View, deltaX: Float, deltaY: Float) {
+    }
 
-    @SuppressWarnings("unused")
-    public void onZoomEnd(View view) { }
+    @Suppress("unused")
+    open fun onDragEnd(view: View, motionEvent: MotionEvent) {
+    }
+
+    @Suppress("unused")
+    open fun onZoom(view: View, ratio: Float) {
+    }
+
+    @Suppress("unused")
+    open fun onZoomEnd(view: View) {
+    }
+
+    companion object {
+        private const val DISTANCE_NO_MOVE = 25
+        private const val DISTANCE_SCROLL = 45
+        private const val CLICK_TIME_THRESHOLD: Long = 200
+    }
 }
 
