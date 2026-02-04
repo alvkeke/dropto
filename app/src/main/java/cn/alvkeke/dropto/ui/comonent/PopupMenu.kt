ed71ab5ccb21a6cd86ae99bb4cacf152164d6fc8
alvkeke <15010199+alvkeke@users.noreply.github.com>
2026-02-05 01:38:27 +0800

    [ui] support menuItem click event, and add animation

--- app/src/main/java/cn/alvkeke/dropto/ui/comonent/PopupMenu.kt
+++ app/src/main/java/cn/alvkeke/dropto/ui/comonent/PopupMenu.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
@@ -14,6 +15,10 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.core.view.size
 import cn.alvkeke.dropto.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 @SuppressLint("ViewConstructor")
 class PopupMenu @JvmOverloads constructor(
@@ -56,7 +61,15 @@ class PopupMenu @JvmOverloads constructor(
                 context.resources.displayMetrics.density
     }
 
+    private val highlightPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = ContextCompat.getColor(context, R.color.popup_menu_item_selected)
+    }
+
     private val menuView = object : View(context) {
+
+        private var downIndex = -1
+        private var animaRatio = 0f
+
         override fun onDraw(canvas: Canvas) {
             super.onDraw(canvas)
             canvas.translate(SHADOW_PADDING.toFloat(), SHADOW_PADDING.toFloat())
@@ -68,6 +81,17 @@ class PopupMenu @JvmOverloads constructor(
                 radius, radius, backgroundPaint
             )
 
+            if (downIndex in 0 until menu.size) {
+                val top = downIndex * menuItemHeight
+                val bottom = top + menuItemHeight
+                highlightPaint.alpha = (animaRatio.coerceIn(0f, 1f) * 255).toInt()
+                canvas.drawRoundRect(
+                    0f, top,
+                    menuWidth, bottom,
+                    radius, radius, highlightPaint
+                )
+            }
+
             for (i in 0 until menu.size) {
                 val menuItem = menu[i]
                 val text = menuItem.title.toString()
@@ -75,6 +99,98 @@ class PopupMenu @JvmOverloads constructor(
                 val y = (i + 0.5f) * menuItemHeight + (itemTextPaint.textSize / 2 - itemTextPaint.descent())
                 canvas.drawText(text, x, y, itemTextPaint)
             }
+        }
+
+        private fun getTouchedMenuItemIndex(x: Float, y: Float): Int {
+            if (x !in 0f..menuWidth || y < 0 || y > menuHeight) {
+                return -1
+            }
+            return (y / menuItemHeight).toInt()
+        }
+
+        private var animateEnterJob: Job? = null
+        private var startTime = 0L
+        private var animationIn = true
+        private val animationInterval = 200
+        private fun animateEnterStart() {
+            animateEnterJob?.cancel()
+            animationIn = true
+            startTime = System.currentTimeMillis()
+            animateEnterJob = CoroutineScope(Dispatchers.Main).launch {
+                while(true) {
+                    val currentTime = System.currentTimeMillis()
+                    val diff = currentTime - startTime
+                    animaRatio = diff / animationInterval.toFloat()
+                    invalidate()
+                    if (animaRatio >= 1f) break
+                    kotlinx.coroutines.delay(15L)
+                }
+            }
+        }
+
+        @Volatile private var animateExitJob: Job? = null
+        private fun animateExitStart() {
+            if (animateExitJob?.isActive == true) return
+            Log.e(TAG, "animate out start")
+            animateExitJob = CoroutineScope(Dispatchers.Main).launch {
+                animateEnterJob?.join()
+
+                animationIn = false
+                startTime = System.currentTimeMillis()
+                while(true) {
+                    val currentTime = System.currentTimeMillis()
+                    val diff = currentTime - startTime
+                    animaRatio = (1f - diff / animationInterval.toFloat())
+                            .coerceAtLeast(0f)
+                    invalidate()
+                    if (diff >= animationInterval) break
+                    kotlinx.coroutines.delay(15L)
+                }
+                animateExitJob = null
+            }
+        }
+
+        @Volatile private var outCount = 0
+        @SuppressLint("ClickableViewAccessibility")
+        override fun onTouchEvent(event: MotionEvent): Boolean {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    downIndex = getTouchedMenuItemIndex(
+                        event.x - SHADOW_PADDING,
+                        event.y - SHADOW_PADDING
+                    )
+                    outCount = 0
+                    animateEnterStart()
+                    return true
+                }
+                MotionEvent.ACTION_UP -> {
+                    val index = getTouchedMenuItemIndex(
+                        event.x - SHADOW_PADDING,
+                        event.y - SHADOW_PADDING
+                    )
+                    if (index == downIndex &&
+                        outCount == 0 &&
+                        index in 0 until menu.size) {
+                        val menuItem = menu[index]
+                        animateExitStart()
+                        menuListener?.onMenuItemClick(menuItem)
+                        dismiss()
+                    }
+                    return true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val index = getTouchedMenuItemIndex(
+                        event.x - SHADOW_PADDING,
+                        event.y - SHADOW_PADDING
+                    )
+                    if (index != downIndex) {
+                        if (outCount < 10) outCount++           // prevent overflow
+                        if (outCount == 1) animateExitStart()    // run only once
+                    }
+                    return true
+                }
+            }
+            return super.onTouchEvent(event)
         }
 
     }
