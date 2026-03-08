3daf420d33ac9520555e01085ffc0ba1376ab193
alvkeke <15010199+alvkeke@users.noreply.github.com>
2026-03-09 00:16:00 +0800

    [code] remove the unneeded inflate

--- app/src/main/java/cn/alvkeke/dropto/ui/adapter/NoteListAdapter.kt
+++ app/src/main/java/cn/alvkeke/dropto/ui/adapter/NoteListAdapter.kt
@@ -1,22 +1,22 @@
 package cn.alvkeke.dropto.ui.adapter
 
 import android.annotation.SuppressLint
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import cn.alvkeke.dropto.R
 import cn.alvkeke.dropto.data.NoteItem
 import cn.alvkeke.dropto.ui.comonent.NoteItemView
 
 class NoteListAdapter : FilterableListAdapter<NoteItem, NoteListAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val content: NoteItemView = itemView.findViewById(R.id.rlist_item_note_content)
-    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.rlist_item_note, parent, false)
+        val view = NoteItemView(parent.context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        }
         return ViewHolder(view)
     }
 
@@ -37,7 +37,7 @@ class NoteListAdapter : FilterableListAdapter<NoteItem, NoteListAdapter.ViewHold
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val note = this.get(position)
-        val view = holder.content
+        val view = holder.itemView as NoteItemView
         view.index = position
         view.text = note.text
         view.createTime = note.createTime
